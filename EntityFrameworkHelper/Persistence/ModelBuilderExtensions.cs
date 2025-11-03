8715b9b2e39a45354126e9f4ab79eb91d51e7359
JavidanValiyev <valiyev.javidan@gmail.com>
2025-11-04 00:01:41 +0400

    All changes

---
+++ EntityFrameworkHelper/Persistence/ModelBuilderExtensions.cs
@@ -0,0 +1,51 @@
+using System.Linq.Expressions;
+using EntityFrameworkHelper.Contracts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace EntityFrameworkHelper.Persistence;
+
+public static class ModelBuilderExtensions
+{
+    /// <summary>
+    /// For adding global filters to queries
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <param name="tenantId"></param>
+    /// <typeparam name="TTenantIdType"></typeparam>
+    public static void AddGlobalFilters<TTenantIdType>(this ModelBuilder builder, TTenantIdType tenantId)
+        where TTenantIdType : struct, IComparable
+    {
+        foreach (var entityType in builder.Model.GetEntityTypes())
+        {
+            var clrType = entityType.ClrType;
+            var parameter = Expression.Parameter(clrType, "e");
+            Expression? combinedFilter = null;
+
+            // Tenant Filter
+            if (typeof(ITenant<TTenantIdType>).IsAssignableFrom(clrType))
+            {
+                var tenantProperty = Expression.Property(parameter, nameof(ITenant<TTenantIdType>.TenantId));
+                var tenantEquals = Expression.Equal(tenantProperty, Expression.Constant(tenantId));
+                combinedFilter = Combine(combinedFilter, tenantEquals);
+            }
+
+            // Soft Delete Filter
+            if (typeof(ISoftDeletable).IsAssignableFrom(clrType))
+            {
+                var isDeletedProperty = Expression.Property(parameter, nameof(ISoftDeletable.IsDeleted));
+                var notDeleted = Expression.Not(isDeletedProperty);
+                combinedFilter = Combine(combinedFilter, notDeleted);
+            }
+
+            if (combinedFilter == null) continue;
+            var lambda = Expression.Lambda(combinedFilter, parameter);
+            builder.Entity(clrType).HasQueryFilter(lambda);
+        }
+    }
+
+    private static Expression? Combine(Expression? current, Expression next)
+    {
+        return current == null ? next : Expression.AndAlso(current, next);
+    }
+}
